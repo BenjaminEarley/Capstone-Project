580870264979ec56c13d9685d147ca3ba14012a9
Benjamin Earley <bearley@outsitenetworks.com>
2016-04-12 19:43:41 -0400

    Adding Google Analytics

---
+++ app/src/main/java/com/benjaminearley/mysubs/App.java
@@ -0,0 +1,13 @@
+package com.benjaminearley.mysubs;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AnalyticsTrackers.initialize(this);
+    }
+}
