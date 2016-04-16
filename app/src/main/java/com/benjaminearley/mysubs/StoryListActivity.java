365e05061b14290e39cd924dd0dda76748e53553
Benjamin Earley <bearley@outsitenetworks.com>
2016-04-16 13:50:52 -0400

    Adding bottom fragment on fab click

--- app/src/main/java/com/benjaminearley/mysubs/StoryListActivity.java
+++ app/src/main/java/com/benjaminearley/mysubs/StoryListActivity.java
@@ -3,6 +3,7 @@ package com.benjaminearley.mysubs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -51,8 +52,8 @@ public class StoryListActivity extends AppCompatActivity {
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    BottomSheetDialogFragment bottomSheetDialogFragment = new SubredditBottomSheetDialogFragment();
+                    bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
                 }
             });
         }
