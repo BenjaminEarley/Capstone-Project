5d91363dc6c0b7329e10479408bd57d96e2dc1e2
Benjamin Earley <bearley@outsitenetworks.com>
2016-04-20 20:31:07 -0400

    Adding recyclerView to sheetDialog for pop up

--- app/src/main/java/com/benjaminearley/mysubs/SubredditBottomSheetDialogFragment.java
+++ app/src/main/java/com/benjaminearley/mysubs/SubredditBottomSheetDialogFragment.java
@@ -12,25 +12,28 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.widget.ListViewCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.benjaminearley.mysubs.data.MySubsContract;
 
 public class SubredditBottomSheetDialogFragment extends BottomSheetDialogFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    static final int COLUMN_TITLE = 2;
     private static final int SUBREDDIT_LOADER = 0;
-
     private static final String[] SUBREDDIT_COLUMNS = {
             MySubsContract.SubredditEntry.TABLE_NAME + "." + MySubsContract.SubredditEntry._ID,
             MySubsContract.SubredditEntry.COLUMN_TITLE,
             MySubsContract.SubredditEntry.COLUMN_URL
     };
-
-    SimpleCursorAdapter adapter;
     ImageButton addButton;
+    SimpleAdapter subredditAdapter;
+    RecyclerView recyclerView;
 
     private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
 
@@ -60,7 +63,14 @@ public class SubredditBottomSheetDialogFragment extends BottomSheetDialogFragmen
             ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
         }
 
-        ListViewCompat listView = (ListViewCompat) contentView.findViewById(R.id.subreddit_list);
+        recyclerView = (RecyclerView) contentView.findViewById(R.id.subreddit_list);
+        subredditAdapter = new SimpleAdapter(new SimpleAdapter.SimpleAdapterOnClickHandler() {
+            @Override
+            public void onClick(String subreddit, SimpleAdapter.ViewHolder vh) {
+                Toast.makeText(getContext(), subreddit, Toast.LENGTH_LONG).show();
+            }
+        });
+        recyclerView.setAdapter(subredditAdapter);
 
         addButton = (ImageButton) contentView.findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -75,16 +85,7 @@ public class SubredditBottomSheetDialogFragment extends BottomSheetDialogFragmen
             }
         });
 
-        String[] from = new String[]{
-                MySubsContract.SubredditEntry.COLUMN_TITLE
-        };
 
-
-        int[] to = new int[]{android.R.id.text1};
-
-        adapter = new SimpleCursorAdapter(contentView.getContext(), android.R.layout.simple_list_item_1, null, from, to, SimpleCursorAdapter.NO_SELECTION);
-
-        listView.setAdapter(adapter);
     }
 
     @Override
@@ -111,11 +112,68 @@ public class SubredditBottomSheetDialogFragment extends BottomSheetDialogFragmen
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        adapter.swapCursor(data);
+        subredditAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        adapter.swapCursor(null);
+        subredditAdapter.swapCursor(null);
+    }
+
+    private static class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
+
+        final private SimpleAdapterOnClickHandler mClickHandler;
+        private Cursor mCursor;
+
+        public SimpleAdapter(SimpleAdapterOnClickHandler mClickHandler) {
+            this.mClickHandler = mClickHandler;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            mCursor.moveToPosition(position);
+            holder.mTextView.setText(mCursor.getString(COLUMN_TITLE));
+        }
+
+        @Override
+        public int getItemCount() {
+            if (null == mCursor) return 0;
+            return mCursor.getCount();
+        }
+
+        public void swapCursor(Cursor newCursor) {
+            mCursor = newCursor;
+            if (newCursor != null) {
+                notifyDataSetChanged();
+            }
+        }
+
+        public interface SimpleAdapterOnClickHandler {
+            void onClick(String subreddit, ViewHolder vh);
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            private TextView mTextView;
+
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mTextView = (TextView) itemView.findViewById(android.R.id.text1);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                mClickHandler.onClick(mCursor.getString(COLUMN_TITLE), this);
+            }
+        }
     }
 }
