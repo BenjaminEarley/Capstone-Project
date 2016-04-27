9a3b1742d7c6c328283387421366d39540c0ccd3
Benjamin Earley <bearley@outsitenetworks.com>
2016-04-27 11:41:47 -0400

    Adding Fabric

--- app/src/main/java/com/benjaminearley/mysubs/App.java
+++ app/src/main/java/com/benjaminearley/mysubs/App.java
@@ -3,13 +3,20 @@ package com.benjaminearley.mysubs;
 import android.app.Application;
 
 import com.benjaminearley.mysubs.sync.MySubsSyncAdapter;
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.analytics.Tracker;
+
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
 
         MySubsSyncAdapter.initializeSyncAdapter(this);
 
